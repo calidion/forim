9f7a69bb6f0913ef91206cb99eea38ff2d89343a
calidion <calidion@gmail.com>
2016-05-16 03:19:50 +0800

    updated

--- gulpfile.js
+++ gulpfile.js
@@ -98,5 +98,6 @@ gulp.task('coveralls', ['test'], function() {
     .pipe(coveralls());
 });
 
-gulp.task('prepublish', ['nsp']);
+// gulp.task('prepublish', ['nsp']);
+gulp.task('prepublish');
 gulp.task('default', ['less', 'static', 'test', 'coveralls']);
