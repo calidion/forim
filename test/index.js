722d45aa053aa1b3ee6f1ef04c14c4f3d0c89fa1
calidion <caldiion@gmail.com>
2016-07-07 02:52:54 +0800

    enabled settings

--- test/index.js
+++ test/index.js
@@ -16,9 +16,9 @@ describe('forim', function () {
   require('./v2/settings');
   require('./v2/express');
   require('./v2/mailer');
+  require('./v2/controller');
   require('./v2/weixin/settings');
   require('./v2/weixin/pages');
   require('./v2/weixin/api');
   require('./v2/weixin/settings.func');
-
 });
---
+++ test/v2/controller.js
@@ -0,0 +1,20 @@
+var assert = require('assert');
+var settings = require('../../lib/v2/controller/settings');
+
+describe('v2 settings', function () {
+  it('should send mailer user activate', function (done) {
+    var func = settings.__onFailed({
+      errorize: function (error, data) {
+        assert(error.code === -1);
+        assert(data === true);
+        done();
+      },
+      errors: {
+        Failed: {
+          code: -1
+        }
+      }
+    });
+    func(true);
+  });
+});
