4e41fcfdbd44729404afab2ffcc3550bfbf243ee
calidion <caldiion@gmail.com>
2016-11-25 23:23:07 +0800

    replaced use account functions with vig

--- test/v2/user.js
+++ test/v2/user.js
@@ -216,6 +216,85 @@ describe('v2 user', function () {
       .end(done);
   });
 
+
+  it('should 200 when get /search_pass', function (done) {
+    var req = http(app);
+    req.get('/search_pass')
+      .expect(200, function (err, res) {
+        res.text.should.containEql('找回密码');
+        done(err);
+      });
+  });
+
+  it('should 200 when get /search_pass', function (done) {
+    var req = http(app);
+    req.get('/v2/password/retrieve')
+      .expect(200, function (err, res) {
+        res.text.should.containEql('找回密码');
+        done(err);
+      });
+  });
+
+  it('should update search pass', function (done) {
+    var req = http(app);
+    req.post('/search_pass')
+      .send({
+        email: email
+      })
+      .expect(200, function (err, res) {
+        res.text.should.containEql('我们已给您填写的电子邮箱发送了一封邮件，请在24小时内点击里面的链接来重置密码。');
+        done(err);
+      });
+  });
+
+  it('should 200 when get /reset_pass', function (done) {
+    app.models.User.findOne({
+      username: username
+    }).then(function (found) {
+      var req = http(app);
+      req.get('/reset_pass')
+        .query({
+          key: found.passwordRetrieveKey,
+          username: username
+        })
+        .expect(200, function (err, res) {
+          res.text.should.containEql('重置密码');
+          done(err);
+        });
+    });
+  });
+  it('should 403 get /reset_pass when with wrong resetKey', function (done) {
+    var req = http(app);
+    req.get('/reset_pass')
+      .query({
+        key: 'wrongkey',
+        username: username
+      })
+      .expect(200, function (err, res) {
+        res.text.should.containEql('信息不匹配或者存在错误，请重新请求！');
+        done(err);
+      });
+  });
+
+  it('should update password', function (done) {
+    app.models.User.findOne({
+      username: username
+    }).then(function (found) {
+      var req = http(app);
+      req.post('/reset_pass')
+        .send({
+          password: 'jkljkljkl',
+          confirm: 'jkljkljkl',
+          key: found.passwordRetrieveKey,
+          username: username
+        })
+        .expect(200, function (err, res) {
+          res.text.should.containEql('你的密码已重置。');
+          done(err);
+        });
+    });
+  });
+
   it('should get user list', function (done) {
     process.env.FORIM_BY_PASS_POLICIES = 1;
     var req = http(app);
