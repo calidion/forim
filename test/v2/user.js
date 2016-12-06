d2bd2d5dd4eb0d89ed8185c77f647b8d19a9274e
eric <eric@ericdeMacBook-Air.local>
2016-12-06 12:27:59 +0800

    updated

--- test/v2/user.js
+++ test/v2/user.js
@@ -45,7 +45,6 @@ describe('v2 user', function () {
         confirm: password
       })
       .expect(200, function (err, res) {
-        console.log(err, res.text);
         assert(!err);
         res.text.should.containEql('欢迎加入');
         done();
