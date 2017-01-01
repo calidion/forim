c82f6eb5843ea6d6314995b032f8cd8324dd4ee0
eric <eric@ericdeMacBook-Air.local>
2017-01-02 03:15:55 +0800

    enabled unblock

--- test/v2/friend/block.js
+++ test/v2/friend/block.js
@@ -49,4 +49,36 @@ describe('friend#block', function () {
       done(err);
     });
   });
+
+  it('should unblock a user', function (done) {
+    process.env.FORIM_BY_PASS_POLICIES = 0;
+    var req = http(app).post('/friend/unblock');
+    req.cookies = shared.cookies;
+    req.send({
+      id: shared.friend.friend.id
+    }).expect(200, function (err, res) {
+      res.body.should.containDeepOrdered({
+        code: 0,
+        message: '成功！',
+        name: 'Success'
+      });
+      done(err);
+    });
+  });
+
+  it('should unblock a user', function (done) {
+    process.env.FORIM_BY_PASS_POLICIES = 0;
+    var req = http(app).post('/friend/unblock');
+    req.cookies = shared.cookies;
+    req.send({
+      id: shared.friend.id
+    }).expect(200, function (err, res) {
+      res.body.should.containDeepOrdered({
+        code: 33554437,
+        message: '用户未找到！',
+        name: 'UserNotFound'
+      });
+      done(err);
+    });
+  });
 });
